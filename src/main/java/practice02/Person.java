f0b861c688043e84bb056f64d2ae0dc3d61bcf7e
MrLinZHui <742847492@qq.com>
2019-07-11 19:34:28 +0800

    finished case2

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,22 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.getName()+". I am " + this.getAge() + " years old.";
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,41 @@
 package practice02;
 
 public class Student {
+    private String name;
+    private int age;
+    private int ClassId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getKlass() {
+        return this.ClassId;
+    }
+
+    public void setClass(int aClass) {
+        ClassId = aClass;
+    }
+
+    public Student(String name, int age, int aClass) {
+        this.name = name;
+        this.age = age;
+        this.ClassId = aClass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + this.getKlass() +".";
+    }
 }
