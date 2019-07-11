cab28e9281e85a261aa629ce3ad0a8f2342d126f
MrLinZHui <742847492@qq.com>
2019-07-11 20:01:40 +0800

    finished case6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.setKlass(0);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(this.getKlass()!=0) {
+            return "My name is "+this.getName() + ". I am " + this.getAge() +" years old. I am a Teacher. I teach Class " + this.getKlass() +".";
+        }
+        return  "My name is "+this.getName() + ". I am " + this.getAge() +" years old. I am a Teacher. I teach No Class.";
+    }
 }
