813597e1fec61ee8a5992e768439ecf5a589df98
MrLinZHui <742847492@qq.com>
2019-07-11 23:14:47 +0800

    finished case9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,42 @@
 package practice09;
 
+import java.io.OutputStream;
+
 public class Klass {
+    private int klass;
+    private String displayName;
+    private Student jerry;
+    public Klass(int klass) {
+        this.klass = klass;
+        jerry = new Student();
+    }
+
+    public int getNumber() {
+        return this.klass;
+    }
+    public void setKlass(int klass){
+        this.klass = klass;
+    }
+    public String getDisplayName() {
+        if(this.getNumber()!=0)
+            return "Class "+this.getNumber();
+        return "";
+    }
+
+    public void assignLeader(Student jerry) {
+       if(jerry.getKlass().getNumber()==this.klass){
+            this.jerry = jerry;
+        }else{
+           System.out.print("It is not one of us.\n");
+       }
+//         return "";
+    }
+
+    public Student getLeader() {
+        return this.jerry;
+    }
+
+    public void appendMember(Student jerry) {
+
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,33 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    public Student(int id,String name, int age,Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+    public Student(){
+
+    }
+
+    @Override
+    public String introduce() {
+        if(this.klass.getLeader().getId()==0) {
+            return "My name is " + this.getName() + ". I am " + this.getAge() + " years old. " + "I am a Student. I am at Class " + this.klass.getNumber() + ".";
+        }
+        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old. " +"I am a Student. I am Leader of Class " + this.klass.getNumber() + ".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
